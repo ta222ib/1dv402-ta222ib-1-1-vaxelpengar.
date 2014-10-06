0744064cd84f451e3660aeebd4c5f115c18cdc3b
ta222ib <ta222ib@student.lnu.se>
2014-10-06 21:46:50 +0200

    finish S01L01

--- 1DV402.S1.L01A/1DV402.S1.L01A/Program.cs
+++ 1DV402.S1.L01A/1DV402.S1.L01A/Program.cs
@@ -20,10 +20,10 @@ namespace _1DV402.S1.L01A
             roundupSum = CalculateRoundupSum(sumOfPurchase);
             roundup = CalculateRoundup(sumOfPurchase, roundupSum);
             paidCash = FetchPaidCash(roundupSum);
-          
-            //OBS! roundup är med en sjuk massa decimaler...
-
-            
+            sumOfChange = CalculateChange(paidCash, roundupSum);
+            WriteReceipt(sumOfPurchase, roundup, roundupSum, paidCash, sumOfChange);
+            CalculateAndWritePhysicalChange(sumOfChange);
+                   
         }
         static double FetchPurchaseInfo() 
         {
@@ -45,17 +45,18 @@ namespace _1DV402.S1.L01A
         }
         static int CalculateRoundupSum(double sum)
         {
-            int roundupSum = (int)Math.Round(sum);
+            int roundupSum = (int)Math.Round(sum, 0, MidpointRounding.AwayFromZero); 
             if (roundupSum <1)
             {
-                Console.WriteLine("Totalsumman är för liten. Köpet kunde inte genomföras");  
+                Console.WriteLine("Totalsumman är för liten. Köpet kunde inte genomföras");
+                Environment.Exit(0);
             }
             return roundupSum;
         }
-        static double CalculateRoundup(double sumOfPurchase2, int roundupSum2)
+        static double CalculateRoundup(double localSumOfPurchase, int localRoundupSum)
         {
             double roundup; 
-            return roundup = sumOfPurchase2 - roundupSum2;
+            return roundup = localRoundupSum - localSumOfPurchase ;
         }
         static int FetchPaidCash(int roundupSum)
         {
@@ -66,21 +67,65 @@ namespace _1DV402.S1.L01A
             if (roundupSum>sum)
                 {
                     Console.WriteLine("Erhållet belopp är för litet. Köpet kunde inte genomföras!");
+                    Environment.Exit(0);
                 }
             return (sum);
         }
+        static int CalculateChange(int localPaidCash, int localRoundupSum)
+        {
+            int localSumOfChange;
+            return localSumOfChange = localPaidCash - localRoundupSum;
+        }
+        static void WriteReceipt(double localSumOfPurchase, double localRoundup, int localRoundupSum, int localPaidCash, int localSumOfChange)
+        {
+            Console.WriteLine("\nKVITTO");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Totalt:\t\t{0}", localSumOfPurchase);
+            Console.WriteLine("Öresavrundning:\t{0}", localRoundup);
+            Console.WriteLine("Att betala:\t{0}", localRoundupSum);
+            Console.WriteLine("Kontant:\t{0}", localPaidCash);
+            Console.WriteLine("Tillbaks:\t{0}", localSumOfChange);
+            Console.WriteLine("-----------------------------------------");
+        }
+        static void CalculateAndWritePhysicalChange (int sumOfChange)
+        {
+            int numberOfFiveHundredBills = sumOfChange / 500;
+            int numberOfHundredBills = sumOfChange % 500 / 100;
+            int numberOfFiftyBills = sumOfChange % 500 % 100 / 50;
+            int numberOfTwentyBills = sumOfChange % 500 % 100 % 50 / 20;
+            int numberOfTenCoins = sumOfChange % 500 % 100 % 50 % 20 / 10;
+            int numberOfFiveCoins = sumOfChange % 500 % 100 % 50 % 20 % 10 / 5;
+            int numberOfOneCoins = sumOfChange % 500 % 100 % 50 % 20 % 10 % 5 / 1;
 
+            if (numberOfFiveHundredBills>0)
+            {
+              Console.WriteLine("500-lappar:\t{0}",numberOfFiveHundredBills);  
+            }
+            if (numberOfHundredBills > 0)
+            {
+              Console.WriteLine("100-lappar:\t{0}", numberOfHundredBills);
+            }
+            if (numberOfFiftyBills>0)
+            {
+                Console.WriteLine("50-lappar:\t{0}", numberOfFiftyBills);
+            }
+            if (numberOfTwentyBills>0)
+            {
+                Console.WriteLine("20-lappar:\t{0}", numberOfTwentyBills);
+            }
+            if (numberOfTenCoins>0)
+            {
+                Console.WriteLine("10-kronor:\t{0}", numberOfTenCoins);
+            }
+            if (numberOfFiveCoins>0)
+            {
+                Console.WriteLine("5-kronor:\t{0}", numberOfFiveCoins);
+            }
+            if (numberOfOneCoins>0)
+            {
+                Console.WriteLine("1-kronor:\t{0}", numberOfOneCoins);
+            }
+            Console.WriteLine("");
+        }
     }
 }
-//fråga efter totalsumma,
-//använd try-catch satser för att ge ny möjlighet att mata in om det är fel, t.ex. text i.s.f. siffror
-//avrunda till närmsta hela krontal
-//hantera om totalsumman är mindre än en krona, 
-//om det blev mindre än 1 SEK, avsluta programmet efter felmeddelande
-//fråga efter erhållet belopp
-//hantera om erhållet belopp är mindre än totalsumman,
-//avsluta programmet efter att felmeddelande visats
-//presentera KVITTO innehållande: "Köpets totala summa", "Öresavrundningen", 
-//"Summa att betala", "Erhållet belopp", "Växel"
-//Dela upp växeln i 500, 100, 50, 20 lappar samt 10, 5 och 1kronorsmynt. 
-//Presentera bara antalet sedlar och mynt som är fler än 0.
